861c2e10e45856b18ad512dfbe2bef409870fb19
Anna <annaarischenko@gmail.com>
2024-10-25 15:19:56 +0300

    edited hw4

--- js_hw4.js
+++ js_hw4.js
@@ -2,10 +2,11 @@ var cat = {
     name: 'Пухнастик',
     age: 5,
     weight: 4.5,
-    getInfo: function () {
+    /*getInfo: function () {
         console.log("Ім'я кота: " + this.name +"\nВік кота: " + this.age + "\nВага кота: " + this.weight)
-    }
+    }*/
 }
 cat.type = "Нова властивість!";
-console.log (cat.type);
-cat.getInfo();
+for (let i in cat) {
+    console.log(cat[i]); 
+  }
